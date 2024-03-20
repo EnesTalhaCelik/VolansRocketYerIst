2e5fe52288840e24642aa584886e93393ad32a35
Enes Talha ÇELİK <enes.talha@hotmail.com.tr>
2024-03-20 17:06:10 +0300

    Shit got done

--- VolansYerIstasyonu/UserControls/uc_PortAyarlari.Designer.cs
+++ VolansYerIstasyonu/UserControls/uc_PortAyarlari.Designer.cs
@@ -31,7 +31,7 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label11 = new System.Windows.Forms.Label();
             this.btnLoraBaglan = new System.Windows.Forms.Button();
-            this.btnLoraBağlantiKes = new System.Windows.Forms.Button();
+            this.btnLoraBaglantiKes = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -62,14 +62,14 @@
             this.label15 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
-            this.comboBox11 = new System.Windows.Forms.ComboBox();
-            this.button10 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.cboxLoraSec = new System.Windows.Forms.ComboBox();
+            this.btnLoraMesajGonder = new System.Windows.Forms.Button();
+            this.txtboxLoraMesaj = new System.Windows.Forms.TextBox();
+            this.btnLoraAgAyarlari = new System.Windows.Forms.Button();
+            this.btnLoraPingGorevY = new System.Windows.Forms.Button();
+            this.btnLoraPingRoket = new System.Windows.Forms.Button();
+            this.btnLoraPingYedekAv = new System.Windows.Forms.Button();
+            this.btnLoraPingPong = new System.Windows.Forms.Button();
             this.logSeriPort = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -80,7 +80,7 @@
             // 
             this.groupBox1.Controls.Add(this.label11);
             this.groupBox1.Controls.Add(this.btnLoraBaglan);
-            this.groupBox1.Controls.Add(this.btnLoraBağlantiKes);
+            this.groupBox1.Controls.Add(this.btnLoraBaglantiKes);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label2);
@@ -117,15 +117,16 @@
             this.btnLoraBaglan.UseVisualStyleBackColor = true;
             this.btnLoraBaglan.Click += new System.EventHandler(this.btnLoraBaglan_Click);
             // 
-            // btnLoraBağlantiKes
+            // btnLoraBaglantiKes
             // 
-            this.btnLoraBağlantiKes.Enabled = false;
-            this.btnLoraBağlantiKes.Location = new System.Drawing.Point(187, 261);
-            this.btnLoraBağlantiKes.Name = "btnLoraBağlantiKes";
-            this.btnLoraBağlantiKes.Size = new System.Drawing.Size(113, 23);
-            this.btnLoraBağlantiKes.TabIndex = 5;
-            this.btnLoraBağlantiKes.Text = "Bağlantıyı Kes";
-            this.btnLoraBağlantiKes.UseVisualStyleBackColor = true;
+            this.btnLoraBaglantiKes.Enabled = false;
+            this.btnLoraBaglantiKes.Location = new System.Drawing.Point(187, 261);
+            this.btnLoraBaglantiKes.Name = "btnLoraBaglantiKes";
+            this.btnLoraBaglantiKes.Size = new System.Drawing.Size(113, 23);
+            this.btnLoraBaglantiKes.TabIndex = 5;
+            this.btnLoraBaglantiKes.Text = "Bağlantıyı Kes";
+            this.btnLoraBaglantiKes.UseVisualStyleBackColor = true;
+            this.btnLoraBaglantiKes.Click += new System.EventHandler(this.btnLoraBaglantiKes_Click);
             // 
             // label4
             // 
@@ -254,6 +255,7 @@
             this.btnHYIBaglan.TabIndex = 5;
             this.btnHYIBaglan.Text = "Bağlan";
             this.btnHYIBaglan.UseVisualStyleBackColor = true;
+            this.btnHYIBaglan.Click += new System.EventHandler(this.btnHYIBaglan_Click);
             // 
             // btnHYIBaglantiKes
             // 
@@ -263,6 +265,7 @@
             this.btnHYIBaglantiKes.TabIndex = 5;
             this.btnHYIBaglantiKes.Text = "Bağlantıyı Kes";
             this.btnHYIBaglantiKes.UseVisualStyleBackColor = true;
+            this.btnHYIBaglantiKes.Click += new System.EventHandler(this.btnHYIBaglantiKes_Click);
             // 
             // label6
             // 
@@ -358,14 +361,14 @@
             this.groupBox3.Controls.Add(this.label15);
             this.groupBox3.Controls.Add(this.label14);
             this.groupBox3.Controls.Add(this.label13);
-            this.groupBox3.Controls.Add(this.comboBox11);
-            this.groupBox3.Controls.Add(this.button10);
-            this.groupBox3.Controls.Add(this.textBox2);
-            this.groupBox3.Controls.Add(this.button9);
-            this.groupBox3.Controls.Add(this.button8);
-            this.groupBox3.Controls.Add(this.button6);
-            this.groupBox3.Controls.Add(this.button7);
-            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.cboxLoraSec);
+            this.groupBox3.Controls.Add(this.btnLoraMesajGonder);
+            this.groupBox3.Controls.Add(this.txtboxLoraMesaj);
+            this.groupBox3.Controls.Add(this.btnLoraAgAyarlari);
+            this.groupBox3.Controls.Add(this.btnLoraPingGorevY);
+            this.groupBox3.Controls.Add(this.btnLoraPingRoket);
+            this.groupBox3.Controls.Add(this.btnLoraPingYedekAv);
+            this.groupBox3.Controls.Add(this.btnLoraPingPong);
             this.groupBox3.Location = new System.Drawing.Point(3, 364);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(690, 176);
@@ -418,81 +421,89 @@
             this.label13.TabIndex = 6;
             this.label13.Text = "Mesaj Gönderilecek Lora\'yı Seçiniz";
             // 
-            // comboBox11
+            // cboxLoraSec
             // 
-            this.comboBox11.FormattingEnabled = true;
-            this.comboBox11.Location = new System.Drawing.Point(452, 55);
-            this.comboBox11.Name = "comboBox11";
-            this.comboBox11.Size = new System.Drawing.Size(121, 21);
-            this.comboBox11.TabIndex = 5;
+            this.cboxLoraSec.Enabled = false;
+            this.cboxLoraSec.FormattingEnabled = true;
+            this.cboxLoraSec.Location = new System.Drawing.Point(452, 55);
+            this.cboxLoraSec.Name = "cboxLoraSec";
+            this.cboxLoraSec.Size = new System.Drawing.Size(121, 21);
+            this.cboxLoraSec.TabIndex = 5;
             // 
-            // button10
+            // btnLoraMesajGonder
             // 
-            this.button10.Location = new System.Drawing.Point(452, 113);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(103, 23);
-            this.button10.TabIndex = 4;
-            this.button10.Text = "Mesaj Gönder";
-            this.button10.UseVisualStyleBackColor = true;
+            this.btnLoraMesajGonder.Enabled = false;
+            this.btnLoraMesajGonder.Location = new System.Drawing.Point(452, 113);
+            this.btnLoraMesajGonder.Name = "btnLoraMesajGonder";
+            this.btnLoraMesajGonder.Size = new System.Drawing.Size(103, 23);
+            this.btnLoraMesajGonder.TabIndex = 4;
+            this.btnLoraMesajGonder.Text = "Mesaj Gönder";
+            this.btnLoraMesajGonder.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // txtboxLoraMesaj
             // 
-            this.textBox2.Location = new System.Drawing.Point(452, 82);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 20);
-            this.textBox2.TabIndex = 3;
+            this.txtboxLoraMesaj.Enabled = false;
+            this.txtboxLoraMesaj.Location = new System.Drawing.Point(452, 82);
+            this.txtboxLoraMesaj.Name = "txtboxLoraMesaj";
+            this.txtboxLoraMesaj.Size = new System.Drawing.Size(121, 20);
+            this.txtboxLoraMesaj.TabIndex = 3;
             // 
-            // button9
+            // btnLoraAgAyarlari
             // 
-            this.button9.Location = new System.Drawing.Point(238, 113);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(75, 23);
-            this.button9.TabIndex = 2;
-            this.button9.Text = "Ağ Ayarları";
-            this.button9.UseVisualStyleBackColor = true;
+            this.btnLoraAgAyarlari.Enabled = false;
+            this.btnLoraAgAyarlari.Location = new System.Drawing.Point(238, 113);
+            this.btnLoraAgAyarlari.Name = "btnLoraAgAyarlari";
+            this.btnLoraAgAyarlari.Size = new System.Drawing.Size(75, 23);
+            this.btnLoraAgAyarlari.TabIndex = 2;
+            this.btnLoraAgAyarlari.Text = "Ağ Ayarları";
+            this.btnLoraAgAyarlari.UseVisualStyleBackColor = true;
             // 
-            // button8
+            // btnLoraPingGorevY
             // 
-            this.button8.Location = new System.Drawing.Point(335, 113);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(95, 23);
-            this.button8.TabIndex = 1;
-            this.button8.Text = "Ping Görev Y.";
-            this.button8.UseVisualStyleBackColor = true;
+            this.btnLoraPingGorevY.Enabled = false;
+            this.btnLoraPingGorevY.Location = new System.Drawing.Point(335, 113);
+            this.btnLoraPingGorevY.Name = "btnLoraPingGorevY";
+            this.btnLoraPingGorevY.Size = new System.Drawing.Size(95, 23);
+            this.btnLoraPingGorevY.TabIndex = 1;
+            this.btnLoraPingGorevY.Text = "Ping Görev Y.";
+            this.btnLoraPingGorevY.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // btnLoraPingRoket
             // 
-            this.button6.Location = new System.Drawing.Point(335, 55);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(95, 23);
-            this.button6.TabIndex = 1;
-            this.button6.Text = "Ping Roket";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnLoraPingRoket.Enabled = false;
+            this.btnLoraPingRoket.Location = new System.Drawing.Point(335, 55);
+            this.btnLoraPingRoket.Name = "btnLoraPingRoket";
+            this.btnLoraPingRoket.Size = new System.Drawing.Size(95, 23);
+            this.btnLoraPingRoket.TabIndex = 1;
+            this.btnLoraPingRoket.Text = "Ping Roket";
+            this.btnLoraPingRoket.UseVisualStyleBackColor = true;
             // 
-            // button7
+            // btnLoraPingYedekAv
             // 
-            this.button7.Location = new System.Drawing.Point(335, 84);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(95, 23);
-            this.button7.TabIndex = 0;
-            this.button7.Text = "Ping Yedek Av.";
-            this.button7.UseVisualStyleBackColor = true;
+            this.btnLoraPingYedekAv.Enabled = false;
+            this.btnLoraPingYedekAv.Location = new System.Drawing.Point(335, 84);
+            this.btnLoraPingYedekAv.Name = "btnLoraPingYedekAv";
+            this.btnLoraPingYedekAv.Size = new System.Drawing.Size(95, 23);
+            this.btnLoraPingYedekAv.TabIndex = 0;
+            this.btnLoraPingYedekAv.Text = "Ping Yedek Av.";
+            this.btnLoraPingYedekAv.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btnLoraPingPong
             // 
-            this.button5.Location = new System.Drawing.Point(335, 26);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(95, 23);
-            this.button5.TabIndex = 0;
-            this.button5.Text = "Ping Pong Test";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnLoraPingPong.Enabled = false;
+            this.btnLoraPingPong.Location = new System.Drawing.Point(335, 26);
+            this.btnLoraPingPong.Name = "btnLoraPingPong";
+            this.btnLoraPingPong.Size = new System.Drawing.Size(95, 23);
+            this.btnLoraPingPong.TabIndex = 0;
+            this.btnLoraPingPong.Text = "Ping Pong Test";
+            this.btnLoraPingPong.UseVisualStyleBackColor = true;
             // 
             // logSeriPort
             // 
             this.logSeriPort.BackColor = System.Drawing.SystemColors.Desktop;
             this.logSeriPort.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.logSeriPort.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.logSeriPort.Location = new System.Drawing.Point(700, 4);
+            this.logSeriPort.Location = new System.Drawing.Point(703, 3);
             this.logSeriPort.Multiline = true;
             this.logSeriPort.Name = "logSeriPort";
             this.logSeriPort.ScrollBars = System.Windows.Forms.ScrollBars.Both;
@@ -535,7 +546,7 @@
         private System.Windows.Forms.ComboBox cboxLoraSP;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox cboxLoraDataBit;
-        private System.Windows.Forms.Button btnLoraBağlantiKes;
+        private System.Windows.Forms.Button btnLoraBaglantiKes;
         private System.Windows.Forms.Button btnLoraBaglan;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button btnHYIBaglan;
@@ -555,14 +566,14 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.ComboBox comboBox11;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox cboxLoraSec;
+        private System.Windows.Forms.Button btnLoraMesajGonder;
+        private System.Windows.Forms.TextBox txtboxLoraMesaj;
+        private System.Windows.Forms.Button btnLoraAgAyarlari;
+        private System.Windows.Forms.Button btnLoraPingGorevY;
+        private System.Windows.Forms.Button btnLoraPingRoket;
+        private System.Windows.Forms.Button btnLoraPingYedekAv;
+        private System.Windows.Forms.Button btnLoraPingPong;
         private System.Windows.Forms.Label label17;
         private System.Windows.Forms.Label label16;
         private System.Windows.Forms.Label label15;
--- VolansYerIstasyonu/UserControls/uc_PortAyarlari.cs
+++ VolansYerIstasyonu/UserControls/uc_PortAyarlari.cs
@@ -75,12 +75,22 @@ namespace VolansYerIstasyonu.UserControls
                 loraSerialPort.Open();
                 MessageBox.Show($"Port Başarıyla Açıldı: ", "Port Başarıyla Açıldı", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 btnLoraBaglan.Enabled = false;
-                btnLoraBağlantiKes.Enabled = true;
+                btnLoraBaglantiKes.Enabled = true;
                 cboxLoraSP.Enabled = false;
                 cboxLoraBaudRate.Enabled = false;
                 cboxLoraDataBit.Enabled = false;
                 cboxLoraStopBit.Enabled = false;
                 cboxLoraParity.Enabled = false;
+
+                btnLoraAgAyarlari.Enabled = true;
+                btnLoraPingGorevY.Enabled = true;
+                btnLoraPingPong.Enabled = true;
+                btnLoraPingRoket.Enabled = true;
+                btnLoraPingYedekAv.Enabled = true;
+                btnLoraMesajGonder.Enabled = true;
+                cboxLoraSec.Enabled = true;
+                txtboxLoraMesaj.Enabled = true;
+
             }
             catch (Exception ex)
             {
@@ -88,6 +98,89 @@ namespace VolansYerIstasyonu.UserControls
             }
 
         }
+
+        private void btnHYIBaglan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HYISerialPort.BaudRate = (int)cboxHYIBaudRate.SelectedItem;
+                HYISerialPort.PortName = cboxHYISP.Text;
+
+                HYISerialPort.Parity = (Parity)cboxHYIParity.SelectedItem;
+                HYISerialPort.DataBits = (int)cboxHYIDataBit.SelectedItem;
+                HYISerialPort.StopBits = (StopBits)(cboxHYIStopBit.SelectedItem);
+
+                HYISerialPort.Open();
+                MessageBox.Show($"Port Başarıyla Açıldı: ", "Port Başarıyla Açıldı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                btnHYIBaglan.Enabled = false;
+                btnHYIBaglantiKes.Enabled = true;
+                cboxHYISP.Enabled = false;
+                cboxHYIBaudRate.Enabled = false;
+                cboxHYIDataBit.Enabled = false;
+                cboxHYIStopBit.Enabled = false;
+                cboxHYIParity.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir Hata Meydana Geldi.: {ex.Message}", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnLoraBaglantiKes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                loraSerialPort.Close();
+                btnLoraBaglan.Enabled = true;
+                btnLoraBaglantiKes.Enabled = false;
+
+                cboxLoraSP.Enabled = true;
+                cboxLoraParity.Enabled = true;
+                cboxLoraDataBit.Enabled = true;
+                cboxLoraBaudRate.Enabled = true;
+                cboxLoraStopBit.Enabled = true;
+
+                btnLoraAgAyarlari.Enabled = false;
+                btnLoraPingGorevY.Enabled = false;
+                btnLoraPingPong.Enabled = false;
+                btnLoraPingRoket.Enabled = false;
+                btnLoraPingYedekAv.Enabled = false;
+                btnLoraMesajGonder.Enabled = false;
+                cboxLoraSec.Enabled = false;
+                txtboxLoraMesaj.Enabled = false;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir Hata Meydana Geldi!: {ex.Message}", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+        private void btnHYIBaglantiKes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HYISerialPort.Close();
+                btnHYIBaglan.Enabled = true;
+                btnHYIBaglantiKes.Enabled = false;
+
+                cboxHYISP.Enabled = true;
+                cboxHYIParity.Enabled = true;
+                cboxHYIDataBit.Enabled = true;
+                cboxHYIBaudRate.Enabled = true;
+                cboxHYIStopBit.Enabled = true;
+
+                
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir Hata Meydana Geldi!: {ex.Message}", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+
 
 
         private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
