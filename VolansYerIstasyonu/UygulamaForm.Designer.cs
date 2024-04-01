01f1289b138b31dbc5429781fc38d06fe71ca25d
Enes Talha ÇELİK <enes.talha@hotmail.com.tr>
2024-04-01 17:03:07 +0300

    PORT AYAR BUTON GÜNCELLEME

--- VolansYerIstasyonu/UygulamaForm.Designer.cs
+++ VolansYerIstasyonu/UygulamaForm.Designer.cs
@@ -34,7 +34,6 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.btn_PrtAyar = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // pnl_AnaSayfa
@@ -56,7 +55,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 250);
+            this.button2.Location = new System.Drawing.Point(12, 186);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(90, 60);
             this.button2.TabIndex = 1;
@@ -66,7 +65,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 315);
+            this.button3.Location = new System.Drawing.Point(12, 252);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(90, 60);
             this.button3.TabIndex = 1;
@@ -75,7 +74,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(12, 380);
+            this.button4.Location = new System.Drawing.Point(12, 317);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(90, 60);
             this.button4.TabIndex = 1;
@@ -92,22 +91,11 @@
             this.btn_PrtAyar.UseVisualStyleBackColor = true;
             this.btn_PrtAyar.Click += new System.EventHandler(this.btn_PrtAyar_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 185);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 60);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Port Ayarları";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btn_PrtAyar_Click);
-            // 
             // UygulamaForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1116, 611);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_PrtAyar);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -129,6 +117,5 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button btn_PrtAyar;
-        private System.Windows.Forms.Button button1;
     }
 }
